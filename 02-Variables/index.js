c57d6d6f04a98481db90bf2b49d77e84d81a4fdb
Gabriel De Lima <130858293+GabrielLima1227@users.noreply.github.com>
2024-09-25 13:25:19 -0300

    test: javaScript Variables

--- 02-Variables/index.js
+++ 02-Variables/index.js
@@ -1,8 +1,33 @@
+// let age = 20;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(`You gpa is: ${gpa}`);
+
+// let firstName = "Gabriel";
+// let favoriteFood = "Pizza";
+// let email = "gab@gmail.com";
+
+// console.log(typeof firstName);
+// console.log(`My first name is ${firstName}`);
+
+// console.log(`My favorite fodd is ${favoriteFood}`);
+// console.log(`Your email is ${email}`);  
+
+// let online = false;
+// let forSale = true;
+// let isStudent = false;
+
+// console.log(`Gab is Online: ${online}`);
+// console.log(`Is this car for sale: ${forSale}`);
+// console.log(`Enrolled: ${isStudent}`);
+
+let fullName = "Gabriel de Lima Sousa";
 let age = 20;
-let price = 10.99;
+let isStudent = false;
 
-console.log(`You are ${age} years old`);
-console.log(`The price is ${price}`);
-
-let firstName = "Gabriel";
-window.alert(firstName);
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled : ${isStudent}`;
