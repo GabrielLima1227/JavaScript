972d8f7ec03fd7295189447f8af34899464ae63a
Gabriel De Lima <130858293+GabrielLima1227@users.noreply.github.com>
2024-09-30 10:11:54 -0300

    test: Learning about Foor Loops

---
+++ 20-For loops 🔂/index.js
@@ -0,0 +1,28 @@
+// For Loop = É uma estrutura de controle que permite executar um bloco de código repetidamente por um número definido de vezes.
+
+// Ex: 01
+
+// for (var i = 10; i > 0; i -= 2) {
+//   console.log(i);
+// }
+// console.log("HAPPY NEW YEAR!");
+
+// Ex:02
+
+// for (var i = 0; i <= 20; i++) {
+//   if (i == 13) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// Ex: 03
+
+for (var i = 0; i <= 20; i++) {
+    if (i == 13) {
+      break;
+    } else {
+      console.log(i);
+    }
+  }
