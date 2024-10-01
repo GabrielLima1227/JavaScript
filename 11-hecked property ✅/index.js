c0b728df402e23102e9136395d13062d43a0a7b3
Gabriel De Lima <130858293+GabrielLima1227@users.noreply.github.com>
2024-10-01 10:55:12 -0300

    test: complete Temperature conversion challenge

--- 11-hecked property ✅/index.js
+++ 11-hecked property ✅/index.js
@@ -9,20 +9,19 @@ const subResult = document.getElementById("subResult");
 const paymentResult = document.getElementById("paymentResult");
 
 mySubmit.addEventListener("click", () => {
-    if(myCheckBox.checked) {
-        subResult.textContent = `You are subscribed!`;
-    }else{
-        subResult.textContent = `You are not subscribed!`;
-    }
+  if (myCheckBox.checked) {
+    subResult.textContent = `You are subscribed!`;
+  } else {
+    subResult.textContent = `You are not subscribed!`;
+  }
 
-    if(visaBtn.checked) {
-        paymentResult.textContent = `You are paying with Visa`;
-    }else if(masterCardBtn.checked){
-        paymentResult.textContent = `You are paying with MasterCard`;
-    }else if(payPalBtn.checked){
-        paymentResult.textContent = `You are paying with PayPal`;
-    }else{
-        paymentResult.textContent = `You must select a payment type`;
-    }
-  
-});    
+  if (visaBtn.checked) {
+    paymentResult.textContent = `You are paying with Visa`;
+  } else if (masterCardBtn.checked) {
+    paymentResult.textContent = `You are paying with MasterCard`;
+  } else if (payPalBtn.checked) {
+    paymentResult.textContent = `You are paying with PayPal`;
+  } else {
+    paymentResult.textContent = `You must select a payment type`;
+  }
+});
---
+++ 24-⭐ Temperature conversion program 🌡️/index.js
@@ -0,0 +1,22 @@
+const textBox = document.getElementById("textBox");
+const toCelsius = document.getElementById("toCelsius");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const btnSubmit = document.getElementById("btnSubmit");
+const resultConversion = document.getElementById("resultConversion");
+
+function convertToFahrenheit(number) {
+  return number * (9 / 5) + 32;
+}
+function convertToCelsius(number) {
+  return ((number - 32) * 5) / 9;
+}
+
+btnSubmit.addEventListener("click", function () {
+  if (toFahrenheit.checked) {
+    resultConversion.textContent = convertToFahrenheit(textBox.value) + "ºF";
+  } else if (toCelsius.checked) {
+    resultConversion.textContent = convertToCelsius(textBox.value)+ "ºC";
+  } else {
+    resultConversion.textContent = "Select a Unit";
+  }
+});
